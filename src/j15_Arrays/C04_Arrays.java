c32867c0d409a56b9303ca34cc731d107985a859
Aliyeyyy <aliyeyyy35@gmail.com>
2022-10-24 21:59:02 +0200

    push

---
+++ src/j15_Arrays/C04_Arrays.java
@@ -0,0 +1,29 @@
+package j15_Arrays;
+
+import java.util.Arrays;
+
+public class C04_Arrays {
+    public static void main(String[] args) {
+
+        // Array esitlik kontrolu... equals methodu  index ve value kontrolu yapar ve true false return eder
+
+
+        int sayi1[] = {39, 3, 72, 63, 84};
+        int sayi2[] = {84, 72, 63, 3, 39};
+
+        System.out.println("sayı1 ve sayı2 eşit mi : " + Arrays.equals(sayi1, sayi2));//false
+
+        Arrays.sort(sayi1);//[3,39,63,72,84]
+        Arrays.sort(sayi2);//[3,39,63,72,84]
+
+        System.out.println("sayı1 ve sayı2 eşit mi : " + Arrays.equals(sayi1, sayi2));//true
+
+        // Array i Stringe cevirme... toString(arr) -> verilen arrayı String data tipine cevirir
+
+        String str=Arrays.toString(sayi1);
+        System.out.println("str = " + str); //[3, 39, 63, 72, 84] array in tum karakterleri string olarak tanımlandı
+        System.out.println(str.substring(6)); //, 63, 72, 84]
+
+
+    }
+}
