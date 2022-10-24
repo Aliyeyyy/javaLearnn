0b5fe45b667cb7a6df3a6d9937cdad270471ab72
Aliyeyyy <aliyeyyy35@gmail.com>
2022-10-24 22:12:29 +0200

    push

---
+++ src/j20_PassByValue/C05_PassByValue.java
@@ -0,0 +1,12 @@
+package j20_PassByValue;
+
+public class C05_PassByValue {
+    public static void main(String[] args) {
+
+     /*
+eger bir method2Da yapılan değişiklik kalıcı olsun istenirse
+1.değişklik  yapılan variable'lar static olarak tanımlanır.
+ */
+
+    }
+}
